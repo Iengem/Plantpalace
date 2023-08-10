b3faa0d0cbd509806508e81c679a7ed8498a9b13
Munachis <momifeofiligo@gmail.com>
2023-08-10 12:56:14 +0100

    Added The Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "../Footer/Footer.css";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <div className="footCont">
+                    <p className="footContHeading">CONTACT INFO</p>
+                    <ul>
+                        <li>ADDRESS <br/> MBM PLAZA, RUMUOMASI,<br/> PORT-HARCOURT </li>
+                        <li>PHONE <br/>+234 APTECH</li>
+                        <li>SUPPORT LINK <br/>customer-support.plantpalace.com</li>
+                        <li>Hello</li>
+                    </ul>
+                </div>
+                <div className="footCont">
+                    <p className="footContHeading">CONTACT INFO</p>
+                    <ul>
+                        <li>ADDRESS <br/> MBM PLAZA, RUMUOMASI,<br/> PORT-HARCOURT </li>
+                        <li>PHONE <br/>+234 APTECH</li>
+                        <li>SUPPORT LINK <br/>customer-support.plantpalace.com</li>
+                        <li>Hello</li>
+                    </ul>
+                </div>
+                <div className="footCont">
+                    <p className="footContHeading">CONTACT INFO</p>
+                    <ul>
+                        <li>ADDRESS <br/> MBM PLAZA, RUMUOMASI,<br/> PORT-HARCOURT </li>
+                        <li>PHONE <br/>+234 APTECH</li>
+                        <li>SUPPORT LINK <br/>customer-support.plantpalace.com</li>
+                        <li>Hello</li>
+                    </ul>
+                </div>
+                <div className="footCont">
+                    <p className="footContHeading">CONTACT INFO</p>
+                    <ul>
+                        <li>ADDRESS <br/> MBM PLAZA, RUMUOMASI,<br/> PORT-HARCOURT </li>
+                        <li>PHONE <br/>+234 APTECH</li>
+                        <li>SUPPORT LINK <br/>customer-support.plantpalace.com</li>
+                        <li>Hello</li>
+                    </ul>
+                </div>
+                
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
