61e241e3c3fd0a227038218a0e8ba1edb38c07da
Munachis <momifeofiligo@gmail.com>
2023-08-11 20:07:11 +0100

    added footer and updated plant page

---
+++ src/components/Plants/Plants.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from '../Navbar/navBar';
+import TopBar from '../topbar/TopBar';
+import Hero from '../hero/hero';
+import PreFooter from '../preFooter/PreFooter';
+import Footer from '../Footer/Footer'
+
+const Plants = () => {
+    return (
+        <div>
+        <NavBar/>
+        <TopBar/>
+        <Hero/>
+        <PreFooter/>
+        <Footer/>
+        </div>
+    );
+}
+
+export default Plants;
