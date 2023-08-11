22cc8e8ccb19e1d72ce6087b96ee3d25a23eb396
Iengem <gaadiiengem@gmail.com>
2023-08-11 10:10:23 +0100

    Slider code added

---
+++ src/components/Homepage/Firstslide.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Tree1 from '../images/eurekaLemon.webp';
+import Tree2 from '../images/Tree2.webp';
+import Tree3 from '../images/Tree3.webp';
+import Tree4 from '../images/Tree4.webp';
+import Tree5 from '../images/Tree5.webp';
+import Tree6 from '../images/Tree6.webp';
+import Tree7 from '../images/Tree7.webp';
+import Tree8 from '../images/Tree8.webp';
+
+export const firstSlide = [
+    {
+        img: <img src={Tree1} alt="tree1" />,
+        name: 'Eureka Lemon TreeWalker',  
+        Price: '$79'
+    },
+    {
+        img: <img src={Tree2} alt="tree2" />,
+        name: 'Variegated Pink Lemon Tree',  
+        Price: '$79'
+    },
+    {
+        img: <img src={Tree3} alt="tree3" />,
+        name: 'Olive Tree',  
+        Price: '$99'
+    },
+    {
+        img: <img src={Tree4} alt="tree4" />,
+        name: 'Bamboo Palm',  
+        Price: '$229'
+    },
+    {
+        img: <img src={Tree5} alt="tree5" />,
+        name: 'Bromeliad Pineapple',  
+        Price: '$79'
+    },
+    {
+        img: <img src={Tree6} alt="tree6" />,
+        name: 'Money Tree',  
+        Price: '$159'
+    },
+    {
+        img: <img src={Tree7} alt="tree7" />,
+        name: 'Red Prayer Plant',  
+        Price: '$49'
+    },
+    {
+        img: <img src={Tree8} alt="tree8" />,
+        name: 'Ficus Altissima',  
+        Price: '$79'
+    }
+]
--- src/components/Homepage/homePage.jsx
+++ src/components/Homepage/homePage.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import './homePage.css';
 import Navbar from '../Navbar/navBar';
 import heroImage from '../images/heroImage.jpg';
+import { firstSlide } from './Firstslide';
 
 const homePage = () =>{
     return(
@@ -10,11 +11,40 @@ const homePage = () =>{
             <Navbar/>
             <div className='content'>
                 <div className='hero'>
-                    <img className='heroImage' src={heroImage}></img>
+                    <img className='heroImage' src={heroImage} alt='heropic'/>
                     <div className='heroText'>
                         <h1>Botany at your Fingertips</h1>
                         <Link className='shopPlants' to='/plant'>Buy Plants</Link>
                     </div>
+                </div>
+                <div className='section1'>
+                    <div className='section1Header'>
+                        <h1>Best Sellers</h1>
+                        <Link to='/plant' className='shopLink'>SHOP ALL</Link>
+                    </div>
+                    <div className='slider'>
+                        <div className='contentSlide'>
+                            {/* Imported the array to be used for the Slider */}
+                          {firstSlide.map((item, key)=>{
+                            return(
+                                <Link className='slideItem'>
+                                    <div className='slideImage'>
+                                        {item.img}
+                                    </div>
+                                    <div className='slideText'>
+                                        <h1>{item.name}</h1>
+                                        <p>{item.Price}</p>
+                                    </div>
+                                </Link>
+                            )
+                          })}
+                        </div>
+                    </div>
+                    <div className='WaysToFind'>
+                        <h1>More Ways to Find Your Perfect Plant</h1>
+                        
+                    </div>
+                
                 </div>
             </div>
         </div>        
