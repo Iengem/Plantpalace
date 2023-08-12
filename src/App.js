7de579c1b3c67712b35a18881d73e45a76da642c
Munachis <momifeofiligo@gmail.com>
2023-08-12 09:41:29 +0100

    routed the plant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Homepage from "./components/Homepage/homePage";
-
+import { Route,Routes,Router } from "react-router";
+import Plants from "./components/Plants/Plants";
 function App() {
   return (
     <div className="App">
-      <Homepage  />
+      <Routes>
+        <Route path="/" element = {<Homepage/>}> </Route>
+        <Route path="/plant" element = {<Plants/>}> </Route>
+      </Routes>
       </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Plants from './components/Plants/Plants';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}/>
+      <Route path='/plant' element =  {<Plants/>}/>
       
     </Routes>
   </BrowserRouter>
